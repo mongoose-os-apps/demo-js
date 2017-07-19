e21455e3de13096f08c2443fd9505cda01cef5d7
Deomid Ryabkov <rojer@cesanta.com>
2017-07-19 09:40:01 +0100

    ESP32 Ethernet support; mgos_net_* API
    Networking API is generalized to mgos_net_*, in particular state change notification and IP address information retrieval.
    
    PUBLISHED_FROM=b312fd620dadc14183640b1caa158d011415b0eb

--- fs/init.js
+++ fs/init.js
@@ -24,7 +24,7 @@ Timer.set(1000 /* 1 sec */, true /* repeat */, function() {
 }, null);
 
 // Publish to MQTT topic on a button press. Button is wired to GPIO pin 0
-GPIO.set_button_handler(0, GPIO.PULL_UP, GPIO.INT_EDGE_NEG, 200, function() {
+GPIO.set_button_handler(34, GPIO.PULL_UP, GPIO.INT_EDGE_NEG, 200, function() {
   let topic = '/devices/' + Cfg.get('device.id') + '/events';
   let message = getInfo();
   let ok = MQTT.pub(topic, message, 1);
