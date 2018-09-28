4171c03d05b7746b612c299c1fef419c858d7669
Sergey Lyubka <lsm@cesanta.com>
2018-09-28 11:51:13 +0100

    Make demo-js shadow-controlled
    CL: Make demo-js shadow-controlled
    
    PUBLISHED_FROM=29948476060139bdc5f874b5fadc0e9ab1de866f

--- fs/init.js
+++ fs/init.js
@@ -1,49 +1,42 @@
 load('api_config.js');
-load('api_events.js');
 load('api_gpio.js');
-load('api_mqtt.js');
-load('api_net.js');
-load('api_sys.js');
+load('api_shadow.js');
 load('api_timer.js');
+load('api_sys.js');
 
-let led = Cfg.get('pins.led');
-let button = Cfg.get('pins.button');
-let topic = '/devices/' + Cfg.get('device.id') + '/events';
+let led = Cfg.get('pins.led');               // Built-in LED GPIO number
+let onhi = Cfg.get('pins.led_active_high');  // LED on when high?
+let state = {on: false};                     // Device state - LED on/off status
 
-print('LED GPIO:', led, 'button GPIO:', button);
+// Set up Shadow handler to synchronise device state with the shadow state
+Shadow.addHandler(function(event, obj) {
+  if (event === 'CONNECTED') {
+    print('Connected to the device shadow');
+    print('  Reporting our current state..');
+    Shadow.update(0, state);  // Report current state. This may generate the
+                              // delta on the cloud, in which case the
+                              // cloud will send UPDATE_DELTA to us
 
-let getInfo = function() {
-  return JSON.stringify({
-    total_ram: Sys.total_ram(),
-    free_ram: Sys.free_ram()
-  });
-};
+    print('  Setting up timer to periodically report metrics..');
+    Timer.set(1000, Timer.REPEAT, function() {
+      let update = {uptime: Sys.uptime()};
+      print(JSON.stringify(update));
+      Shadow.update(0, update);  // Set uptime in seconds in the shadow
+    }, null);
 
-// Blink built-in LED every second
-GPIO.set_mode(led, GPIO.MODE_OUTPUT);
-Timer.set(1000 /* 1 sec */, Timer.REPEAT, function() {
-  let value = GPIO.toggle(led);
-  print(value ? 'Tick' : 'Tock', 'uptime:', Sys.uptime(), getInfo());
-}, null);
-
-// Publish to MQTT topic on a button press. Button is wired to GPIO pin 0
-GPIO.set_button_handler(button, GPIO.PULL_UP, GPIO.INT_EDGE_NEG, 20, function() {
-  let message = getInfo();
-  let ok = MQTT.pub(topic, message, 1);
-  print('Published:', ok, topic, '->', message);
-}, null);
-
-// Monitor network connectivity.
-Event.addGroupHandler(Net.EVENT_GRP, function(ev, evdata, arg) {
-  let evs = '???';
-  if (ev === Net.STATUS_DISCONNECTED) {
-    evs = 'DISCONNECTED';
-  } else if (ev === Net.STATUS_CONNECTING) {
-    evs = 'CONNECTING';
-  } else if (ev === Net.STATUS_CONNECTED) {
-    evs = 'CONNECTED';
-  } else if (ev === Net.STATUS_GOT_IP) {
-    evs = 'GOT_IP';
+  } else if (event === 'UPDATE_DELTA') {
+    print('GOT DELTA:', JSON.stringify(obj));
+    for (let key in obj) {  // Iterate over all keys in delta
+      if (key === 'on') {   // We know about the 'on' key. Handle it!
+        state.on = obj.on;  // Synchronise the state
+        var level = onhi ? state.on : !state.on;
+        GPIO.set_mode(led, GPIO.MODE_OUTPUT);  // And turn on/off the LED
+        GPIO.write(led, level);                // according to the delta
+        print('LED on ->', state.on);
+      } else {
+        print('Dont know how to handle key', key);
+      }
+    }
+    Shadow.update(0, state);  // Report our new state, hopefully clearing delta
   }
-  print('== Net event:', ev, evs);
-}, null);
+});
