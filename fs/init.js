69f63f7f3536aea34e7de0d9425a6befcf27d1f2
Deomid Ryabkov <rojer@cesanta.com>
2018-11-06 17:50:57 +0000

    Cloud connection status logic cleanup
     * Add mgos_xxx_is_connected to all cloud libs
    
     * Raise MGOS_EVENT_CLOUD_{CONNECTED,DISCONNECTED} with appropriate cloud type.
    
     * Add a button handler to demo-js that sends an event to the cloud, using appropriate method depending on cloud type.
    
    CL: Cloud connection status logic cleanup
    
    PUBLISHED_FROM=c466921b09341c0456229f45dada0914c5c1df8f

--- fs/init.js
+++ fs/init.js
@@ -1,14 +1,21 @@
+load('api_aws.js');
+load('api_azure.js');
 load('api_config.js');
+load('api_dash.js');
 load('api_events.js');
+load('api_gcp.js');
 load('api_gpio.js');
+load('api_mqtt.js');
 load('api_shadow.js');
 load('api_timer.js');
 load('api_sys.js');
+load('api_watson.js');
 
-let led = Cfg.get('board.led1.pin');           // Built-in LED GPIO number
-let onhi = Cfg.get('board.led1.active_high');  // LED on when high?
-let state = {on: false, uptime: 0};            // Device state
-let online = false;                            // Connected to the cloud?
+let btn = Cfg.get('board.btn1.pin');              // Built-in button GPIO
+let led = Cfg.get('board.led1.pin');              // Built-in LED GPIO number
+let onhi = Cfg.get('board.led1.active_high');     // LED on when high?
+let state = {on: false, btnCount: 0, uptime: 0};  // Device state
+let online = false;                               // Connected to the cloud?
 
 let setLED = function(on) {
   let level = onhi ? on : !on;
@@ -48,6 +55,51 @@ Shadow.addHandler(function(event, obj) {
     reportState();  // Report our new state, hopefully clearing delta
   }
 });
+
+if (btn >= 0) {
+  let btnCount = 0;
+  let btnPull, btnEdge;
+  if (Cfg.get('board.btn1.pull_up') ? GPIO.PULL_UP : GPIO.PULL_DOWN) {
+    btnPull = GPIO.PULL_UP;
+    btnEdge = GPIO.INT_EDGE_NEG;
+  } else {
+    btnPull = GPIO.PULL_DOWN;
+    btnEdge = GPIO.INT_EDGE_POS;
+  }
+  GPIO.set_button_handler(btn, btnPull, btnEdge, 20, function() {
+    state.btnCount++;
+    let message = JSON.stringify(state);
+    let sendMQTT = true;
+    if (Azure.isConnected()) {
+      print('== Sending Azure D2C message:', message);
+      Azure.sendD2CMsg("", message);
+      sendMQTT = false;
+    }
+    if (GCP.isConnected()) {
+      print('== Sending GCP event:', message);
+      GCP.sendEvent(message);
+      sendMQTT = false;
+    }
+    if (Watson.isConnected()) {
+      print('== Sending Watson event:', message);
+      Watson.sendEventJSON("ev", {d: state});
+      sendMQTT = false;
+    }
+    if (Dash.isConnected()) {
+      print('== Click!');
+      // TODO: Maybe do something else?
+      sendMQTT = false;
+    }
+    // AWS is handled as plain MQTT since it allows arbitrary topics.
+    if (AWS.isConnected() || (MQTT.isConnected() && sendMQTT)) {
+      let topic = 'devices/' + Cfg.get('device.id') + '/events';
+      print('== Publishing to ' + topic + ':', message);
+      MQTT.pub(topic, message, 0 /* QoS */);
+    } else if (sendMQTT) {
+      print('== Not connected!');
+    }
+  }, null);
+}
 
 Event.on(Event.CLOUD_CONNECTED, function() {
   online = true;
